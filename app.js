b98aa1eb21a3bfd5dbbe1e33a2efc725f02b0e34
Bahadır Perveli <bahadirperveli01@gmail.com>
2023-05-15 12:21:27 +0300

    update app.js
    Gerekli olan tüm tablolar güncelleme ile sağlanmış olup diğer tüm nav-link için değişken adları değiştirilip gerekli olan altyapı sağlanmıştır

--- app.js
+++ app.js
@@ -45,3 +45,107 @@ document.getElementById('addMachineButton').addEventListener('click', () => {
     document.getElementById('machineID').value = null;
     document.getElementById('machineForm').style.display = 'block';
 });
+
+document.getElementById('machineForm').addEventListener('submit', (e) => {
+    e.preventDefault();
+    const machineID = document.getElementById('machineID').value;
+    const machineName = document.getElementById('machineName').value;
+    const machineDescription = document.getElementById('machineDescription').value;
+    const machineParentGroupID = document.getElementById('machineParentGroupID').value;
+    if(machineID) {
+        axiosInstance.put(`/api/machines/${machineID}`, {
+            machineID,
+            machineName,
+            machineDescription,
+            machineParentGroupID
+        }).then(loadMachines);
+    } else {
+        axiosInstance.post('/api/machines', {
+            machineName,
+            machineDescription,
+            machineParentGroupID
+        }).then(loadMachines);
+    }
+});
+
+function loadMachines() {
+    axiosInstance.get('/api/machines').then((response) => {
+        const machinesTable = document.getElementById('machinesTable');
+        machinesTable.innerHTML = '';
+        machineParentGroupSelect.innerHTML = ''; // Temizleme işlemi
+        let emptyOption = document.createElement('option');
+        emptyOption.value = ''; // Boş değer
+        emptyOption.textContent = 'No Parent Group'; // Boş seçenek metni
+        machineParentGroupSelect.appendChild(emptyOption);
+        for (let machine of response.data) {
+            let row = document.createElement('tr');
+            row.innerHTML = `
+                <td>${machine.machineID}</td>
+                <td>${machine.machineName}</td>
+                <td>${machine.machineDescription}</td>
+                <td>${getParentObjectName(machine.machineParentGroupID)}</td>
+                <td>
+                    <button class="btn btn-primary" onclick="editMachine(${machine.machineID})">Edit</button>
+                    <button class="btn btn-danger" onclick="deleteMachine(${machine.machineID})">Delete</button>
+                </td>
+            `;
+            machinesTable.appendChild(row);
+
+            // Makine Parent Group ID dropdownunu güncelleme
+            let option = document.createElement('option');
+            option.value = machine.machineID;
+            option.textContent = machine.machineName;
+            machineParentGroupSelect.appendChild(option);
+        }
+    });
+}
+
+function getParentObjectName(parentID) {
+    if (parentID === 0) {
+        return 'No Parent';
+    } else {
+        const parentMachine = machines.find(machine => machine.machineID === parentID);
+        return parentMachine ? parentMachine.machineName : 'Unknown';
+    }
+}
+
+function editMachine(id) {
+    const machine = machines.find(m => m.machineID == id);
+    document.getElementById('machineID').value = machine.machineID;
+    document.getElementById('machineName').value = machine.machineName;
+    document.getElementById('machineDescription').value = machine.machineDescription;
+    document.getElementById('machineParentGroupID').value = machine.machineParentGroupID;
+    document.getElementById('machineForm').style.display = 'block';
+}
+
+function deleteMachine(id) {
+    axiosInstance.delete(`/api/machines/${id}`).then(loadMachines);
+}
+
+const navLinks = document.querySelectorAll('.navbar-nav .nav-link');
+const contentSections = document.querySelectorAll('.content-section');
+
+navLinks.forEach(link => {
+    link.addEventListener('click', e => {
+        e.preventDefault();
+        const targetId = link.getAttribute('href').substring(1);
+        showSection(targetId);
+    });
+});
+
+function showSection(sectionId) {
+    contentSections.forEach(section => {
+        if (section.id === sectionId) {
+            section.style.display = 'block';
+        } else {
+            section.style.display = 'none';
+        }
+    });
+}
+
+loadMachines();
+showSection("machinesSection")
+
+//Machines page
+
+// Products page
