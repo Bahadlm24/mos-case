f274387a18e6d7de94423130f0c2036863dc2b6b
Bahadır Perveli <bahadirperveli01@gmail.com>
2023-05-15 11:40:41 +0300

    Update app.js
    Axios kütüphanesinin Mocklanması için ve local üzerinden girilen değerlerin çekilmesi için gerekli olan veriyi çekmesi sağlanmıştır

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
 const axiosInstance = axios.create();
 const mock = new AxiosMockAdapter(axiosInstance);
 
+let machines = [
+    { machineID: 1, machineName: 'Machine 1', machineDescription: 'Machine 1 description', machineParentGroupID: 0 },
+    { machineID: 2, machineName: 'Machine 2', machineDescription: 'Machine 2 description', machineParentGroupID: 1 }
+];
+
+//Machine Page
+
+mock.onGet('/api/machines').reply(200, machines);
+mock.onPost('/api/machines').reply(config => {
+    const machine = JSON.parse(config.data);
+    machine.machineID = machines.length + 1;
+    machines.push(machine);
+    return [200, machine];
+});
+
+mock.onPut(/\/api\/machines\/\d+/).reply(config => {
+    const updatedMachine = JSON.parse(config.data);
+    const machine = machines.find(m => m.machineID == updatedMachine.machineID);
+    if (machine) {
+        machine.machineName = updatedMachine.machineName;
+        machine.machineDescription = updatedMachine.machineDescription;
+        machine.machineParentGroupID = updatedMachine.machineParentGroupID;
+        return [200, machine];
+    } else {
+        return [404];
+    }
+});
